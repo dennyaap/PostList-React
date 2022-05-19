f8b0ae33a5faa094558eb281cbd4ad1ce9f9d880
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-19 21:19:38 +0300

    added search posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Counter from './components/Counter';
 // import ClassCounter from './components/ClassCounter';
-import {useState, useRef} from 'react';
+import {useState, useRef, useMemo} from 'react';
 import './styles/App.css';
 import PostForm from './components/PostForm';
 import PostList from "./components/PostList";
-import MySelect from './components/UI/select/MySelect';
+import PostFilter from './components/PostFilter';
 
 
 
@@ -15,12 +15,34 @@ function App() {
     {id: 3, title: 'bb', body: 'яя'},
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('');
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  // const [selectedSort, setSelectedSort] = useState('');
+  // const [searchQuery, setSearchQuery] = useState('');
 
   // const [post, setPost] = useState({title: '', body: ''});
   // const [body, setBody] = useState('fddff');
 
   // const bodyInputRef = useRef();
+
+  // function getSortedPosts(){
+  //   console.log('Отработала функция');
+  //   if(selectedSort){
+  //     return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+  //   } 
+  //   return posts;
+  // }
+
+  const sortedPosts = useMemo(() => {
+    console.log('Отработала функция');
+    if(filter.sort){
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    } 
+    return posts;
+  }, [filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts]) //массив зависимостей. будем реагировать только на изменение этих зависимостей
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost]);
@@ -29,25 +51,19 @@ function App() {
     setPosts(posts.filter(item => item.id !== post.id));
     console.log(post.id)
   }
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]))) //мутируем копию массива и сортируем его. Так как на прямую нельзя сортировать массив.
-    console.log(sort);
-  }
+  // const sortPosts = (sort) => {
+  //   setSelectedSort(sort);
+  //   // setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]))) //мутируем копию массива и сортируем его. Так как на прямую нельзя сортировать массив.
+  //   console.log(sort);
+  // }
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <MySelect value={selectedSort} onChange={sortPosts} defaultValue='Сортировка' options={[
-          {value: 'title', name: 'По названию'},
-          {value: 'body', name: 'По описанию'}
-
-        ]}/>
-      </div>
+      <PostFilter filter={filter} setFilter={setFilter}/>
       {/* Условная отрисовка */}
-      {posts.length !== 0
-        ? <PostList posts={posts} title={'Список постов 1'} remove={removePost}/>
+      {sortedAndSearchedPosts.length !== 0
+        ? <PostList posts={sortedAndSearchedPosts} title={'Список постов 1'} remove={removePost}/>
         : <h1 style={{textAlign: 'center'}}>Посты не были найдены!</h1>
       }
     </div>
