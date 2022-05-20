b13b58f536e7646cf3341831e53a2e6e1eb0c4f5
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-20 11:37:18 +0300

    added modal create post

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './styles/App.css';
 import PostForm from './components/PostForm';
 import PostList from "./components/PostList";
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 
 
@@ -16,6 +18,8 @@ function App() {
   ]);
 
   const [filter, setFilter] = useState({sort: '', query: ''});
+
+  const [modal, setModal] = useState(false);
   // const [selectedSort, setSelectedSort] = useState('');
   // const [searchQuery, setSearchQuery] = useState('');
 
@@ -46,6 +50,7 @@ function App() {
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost]);
+    setModal(false);
   }
   const removePost = (post) => {
     setPosts(posts.filter(item => item.id !== post.id));
@@ -58,7 +63,12 @@ function App() {
   // }
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: '30px'}}onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
       {/* Условная отрисовка */}
