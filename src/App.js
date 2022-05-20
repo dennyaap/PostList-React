67975900bee13e3f2bbbc78270a41449e0500036
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-20 15:15:51 +0300

    create hooks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostList from "./components/PostList";
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 
 
@@ -36,17 +37,19 @@ function App() {
   //   return posts;
   // }
 
-  const sortedPosts = useMemo(() => {
-    console.log('Отработала функция');
-    if(filter.sort){
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    } 
-    return posts;
-  }, [filter.sort, posts])
+  // const sortedPosts = useMemo(() => {
+  //   console.log('Отработала функция');
+  //   if(filter.sort){
+  //     return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+  //   } 
+  //   return posts;
+  // }, [filter.sort, posts])
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts]) //массив зависимостей. будем реагировать только на изменение этих зависимостей
+  // const sortedAndSearchedPosts = useMemo(() => {
+  //   return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  // }, [filter.query, sortedPosts]) //массив зависимостей. будем реагировать только на изменение этих зависимостей
+
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost]);
